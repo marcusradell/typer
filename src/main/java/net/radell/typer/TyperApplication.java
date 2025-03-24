b2a9b7a9d1e1006d660ed72b877b1419400e46a2
Marcus Rådell <marcus@radell.net>
2025-03-24 13:28:54 +0100

    init

---
+++ src/main/java/net/radell/typer/TyperApplication.java
@@ -0,0 +1,13 @@
+package net.radell.typer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TyperApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TyperApplication.class, args);
+	}
+
+}
