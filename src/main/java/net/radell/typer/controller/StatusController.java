33b154ed4aa604558803488c2d9095172d1871f6
Marcus Rådell <marcus@radell.net>
2025-03-25 08:56:35 +0100

    refa: put StatusController inside controller package

--- src/main/java/net/radell/typer/controller/StatusController.java
+++ src/main/java/net/radell/typer/controller/StatusController.java
@@ -1,4 +1,4 @@
-package net.radell.typer;
+package net.radell.typer.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
