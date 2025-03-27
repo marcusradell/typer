cf2ddb933c1791eb29f68c53a0e6a443a850f5f7
Marcus Rådell <marcus@radell.net>
2025-03-27 10:54:26 +0100

    feat: use the real db implementation

--- src/main/java/net/radell/typer/service/AttemptsService.java
+++ src/main/java/net/radell/typer/service/AttemptsService.java
@@ -1,19 +1,21 @@
 package net.radell.typer.service;
 
-import net.radell.typer.repository.AttemptsRepositoryOld;
+import net.radell.typer.model.Attempt;
+import net.radell.typer.repository.AttemptsRepository;
 import org.springframework.stereotype.Service;
 
 import java.util.UUID;
 
 @Service
 public class AttemptsService {
-    private final AttemptsRepositoryOld attemptsRepository;
+    private final AttemptsRepository attemptsRepository;
 
-    public AttemptsService(AttemptsRepositoryOld attemptsRepository) {
+    public AttemptsService(AttemptsRepository attemptsRepository) {
         this.attemptsRepository = attemptsRepository;
     }
 
     public UUID create(char letter) {
-        return attemptsRepository.create(letter);
+        var attempt = new Attempt(letter);
+        return attemptsRepository.save(attempt).getId();
     }
 }
